2d8727300660fe35c0ce9f07ccf05c55c479b102
Sagar Patel <sagarpatel1313@gmail.com>
2019-03-22 15:29:14 -0400

    Updated object recognition script to include code to interface with the Arduino Nano via a serial connection

--- Rpi-files/autonomous-tracking-program_updated.py
+++ Rpi-files/autonomous-tracking-program_updated.py
@@ -1,14 +1,20 @@
 import cv2
 import sys
+import serial
+import time 
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 # Program requires an xml file of classification data
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 
 frame_center_x = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)/2)
 frame_center_y = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)/2)
+
+center_width = 150
 
 
 # variables used to keep track of detected object's distance
@@ -33,9 +39,9 @@ while True:
     # Indicate on the frame where the center is using a blue circle
     cv2.circle(frame, (frame_center_x,frame_center_y), 3, (255, 0, 0), 2)
 
+    time.sleep(0.050)
 
     for (x, y, w, h) in faces:
-	
 	# Draw a rectangle around the faces
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
@@ -44,23 +50,33 @@ while True:
 
         x_difference = x + w/2 - frame_center_x
 	size_difference = w - old_size
+	size_difference = w - center_width
 
 	# If the new x-coordinate and old x-coordinate difference exceeds the threshold, rotate the robot accordingly
-        if(abs(x_difference) > 50):
+        if(abs(x_difference) > 130):
 		if(x_difference > 0):
-			print("Rotate left")	
+			print("Rotate left")
+			#ser.write(b'd')
+			ser.write(b'\n')	
 		if(x_difference < 0):
 			print("Rotate right")
+			#ser.write(b'a')
+			ser.write(b'\n')	
 	        #print("Coordinates - x: {}, y {}".format(x,y))
 
 
 	# If the new distance and old distance difference exceeds the threshold, move the robot accordingly
-	if(abs(size_difference) > 20):
+	if(abs(size_difference) > 10):
+	#if(abs(size_difference)*10/(old_size+1) > 1):
 		old_size = w
 		if(size_difference < 0):
 			print("Move closer")
+			ser.write(b'w')
+			ser.write(b'\n')	
 		if(size_difference > 0):
 			print("Move further")
+			ser.write(b's')
+			ser.write(b'\n')	
 	        #print("Width, Height: {}, {}".format(w,h))
 
     # Display the resulting frame
